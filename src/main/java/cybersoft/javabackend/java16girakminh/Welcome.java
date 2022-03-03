6792756c72ec28bb07c87a8d11684cce60991179
minhnk1995 <minhnk1995@gmail.com>
2022-03-03 21:06:43 +0700

    Update Welcome.java

--- src/main/java/cybersoft/javabackend/java16girakminh/Welcome.java
+++ src/main/java/cybersoft/javabackend/java16girakminh/Welcome.java
@@ -1,10 +1,14 @@
 package cybersoft.javabackend.java16girakminh;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class Welcome {
-	@GetMapping
-	public Oject welcome() {
-		return "hello";
+	@GetMapping("/welcome")
+	public Object welcome() {
+		return new ResponseEntity<>("hello", HttpStatus.OK);
 	}
 }
